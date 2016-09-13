b51fc8053fa9b4ae19133a123b9815100d9f0e08
JoaoMarcus <joaomarcusjesus@gmail.com>
2016-09-13 15:14:42 -0300

    Composer Package

---
+++ App/Application.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JoaoMarcus
+ * Date: 13/09/2016
+ * Time: 14:38
+ */
+
+namespace App;
+
+
+class Application
+{
+    public function Index()
+    {
+        return "Hello Wolrd! Composer :)";
+    }
+}
